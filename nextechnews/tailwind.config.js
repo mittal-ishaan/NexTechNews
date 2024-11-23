7ba39b1961dc8f4dff074eb9b8641480045b5a5c
Ishaan Mittal <ishaanmittal123@gmail.com>
2024-11-24 02:19:12 +0530

    start docker work

--- nextechnews/tailwind.config.js
+++ nextechnews/tailwind.config.js
@@ -1,6 +1,6 @@
 import {nextui} from "@nextui-org/react";
+import plugin from "tailwindcss";
 /** @type {import('tailwindcss').Config} */
-const plugin = require('tailwindcss/plugin');
 module.exports = {
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
